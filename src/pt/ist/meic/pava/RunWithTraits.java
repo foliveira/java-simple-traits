a566243fb6dca98bc6dab119efebe1977e5140f4
Fábio Oliveira <fabio.an.oliveira@gmail.com>
2011-03-09 01:25:54 +0000

    Divided annotations in different files.
    Signed-off-by: Fábio Oliveira <fabio.an.oliveira@gmail.com>

--- src/pt/ist/meic/pava/RunWithTraits.java
+++ src/pt/ist/meic/pava/RunWithTraits.java
@@ -1,16 +1,38 @@
 package pt.ist.meic.pava;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class RunWithTraits
 {
 	public static void main(String[] args) 
 	{
-		Class clazz = Class.forName(args[0]);
-		Method mainMethod = clazz.getDeclaredMethod("main", new Class[] { String[].class });
+		Class clazz;
+		Method mainMethod = null;
+		
+		try {
+			clazz = Class.forName(args[0]);
+			mainMethod = clazz.getDeclaredMethod("main", new Class[] { String[].class });
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		}
 		
 		//Inject all Traits code into relevant classes
 		
-		String newArgs = new String[args.length - 1];
-		System.arrayCopy(args, 1, newArgs, 0, newArgs.length);
-		mainMethod.invoke(null, newArgs);
+		String[] newArgs = new String[args.length - 1];
+		System.arraycopy(args, 1, newArgs, 0, newArgs.length);
+		try {
+			mainMethod.invoke(null, newArgs);
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
 	}
 }
